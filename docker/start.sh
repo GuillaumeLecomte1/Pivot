dd703a29c163f14a3b101035c12a173c746dc592
GuillaumeLecomte1 <guillaume.lecomte049@outlook.fr>
2025-04-08 13:50:36 +0200

    J'ai une 502 a cause des env probleme de config

--- docker/start.sh
+++ docker/start.sh
@@ -1,61 +1,7 @@
 #!/bin/sh
 
-# Créer un fichier .env vide
-touch .env
-
-# Ajouter les variables d'environnement de base
-cat > .env << EOL
-APP_NAME=Pivot
-APP_ENV=production
-APP_DEBUG=false
-APP_URL=https://pivot.guillaume-lcte.fr
-ASSET_URL=https://pivot.guillaume-lcte.fr
-FORCE_HTTPS=true
-SESSION_SECURE_COOKIE=true
-SESSION_SAME_SITE=lax
-SESSION_DOMAIN=pivot.guillaume-lcte.fr
-
-LOG_CHANNEL=stack
-LOG_DEPRECATIONS_CHANNEL=null
-LOG_LEVEL=error
-
-DB_CONNECTION=mysql
-DB_HOST=mariadb
-DB_PORT=3306
-DB_DATABASE=pivot
-DB_USERNAME=pivot
-DB_PASSWORD=
-
-BROADCAST_DRIVER=log
-CACHE_DRIVER=file
-FILESYSTEM_DISK=local
-QUEUE_CONNECTION=sync
-SESSION_DRIVER=file
-SESSION_LIFETIME=120
-
-MEMCACHED_HOST=127.0.0.1
-
-REDIS_HOST=127.0.0.1
-REDIS_PASSWORD=null
-REDIS_PORT=6379
-
-MAIL_MAILER=smtp
-MAIL_HOST=mailpit
-MAIL_PORT=1025
-MAIL_USERNAME=null
-MAIL_PASSWORD=null
-MAIL_ENCRYPTION=null
-MAIL_FROM_ADDRESS="hello@example.com"
-MAIL_FROM_NAME="\${APP_NAME}"
-
-VITE_APP_NAME="\${APP_NAME}"
-VITE_APP_URL="\${APP_URL}"
-VITE_PUSHER_APP_KEY="\${PUSHER_APP_KEY}"
-VITE_PUSHER_HOST="\${PUSHER_HOST}"
-VITE_PUSHER_PORT="\${PUSHER_PORT}"
-VITE_PUSHER_SCHEME="\${PUSHER_SCHEME}"
-VITE_PUSHER_APP_CLUSTER="\${PUSHER_APP_CLUSTER}"
-EOL
+# Copier le fichier .env.production vers .env
+cp .env.production .env
 
 # Mettre à jour le fichier .env avec les variables d'environnement de Dokploy
 if [ -n "$APP_URL" ]; then
@@ -73,9 +19,6 @@ fi
 if [ -n "$SESSION_DOMAIN" ]; then
     sed -i "s|^SESSION_DOMAIN=.*|SESSION_DOMAIN=$SESSION_DOMAIN|g" .env
 fi
-
-# Generate app key
-php artisan key:generate --force
 
 # Create supervisor log directory if it doesn't exist
 mkdir -p /var/log/supervisor
